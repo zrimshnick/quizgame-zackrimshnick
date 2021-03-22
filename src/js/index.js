a30e45b47691cda5f46484da41cce55f39380dca
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2021-03-22 12:40:30 -0400

    change parcel

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
 console.log("Connected");
 
-const bootstrap = require("bootstrap");
+//to start project
+let score = {};
 
-/* to start project
-// score questions individually
+// store list of questions in array
+const question = {
+  questionContent: "How do I say hello in Italian?",
+  choices: [
+    {
+      choice: "Caio",
+      correct: true,
+    },
+    {
+      choice: "Ni Hao",
+      correct: false,
+    },
+  ],
+};
 
+// listen for start event, click or page load
 
-// get total score
+// display question
 
+// listen for user answer click
 
-*/
+// if answer is true then update score show true modal green check
+// else update score show false modal red x
+
+// if question number is less than qusetions.length show next question
+// else display results
