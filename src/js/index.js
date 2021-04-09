834ea3a4ebabdc3fd037365fde92aca5c1a8348a
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2021-04-09 12:17:26 -0400

    added questions js folder

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 console.log("Connected");
 ///////////////////////////////////////////////////////////////
+// importing other js files
+import { questions } from "./questions";
+
+///////////////////////////////////////////////////////////////
 // Start js for quiz game here
 
 // grab elements in the HTML by id to use later in the js
@@ -99,47 +103,3 @@ function clearStatusClass(element) {
   element.classList.remove("correct");
   element.classList.remove("wrong");
 }
-
-// List of questions for quiz
-const questions = [
-  {
-    question:
-      "Who led the NBA in scoring with 34.3 ppg in the 2019-2020 season?",
-    answers: [
-      { text: "James Harden", correct: true },
-      { text: "Steph Curry", correct: false },
-      { text: "Bradley Beal", correct: false },
-      { text: "Giannis Antetokounmpo", correct: false },
-    ],
-  },
-  {
-    question:
-      "Who led the NBA in rebounding with 15.2 rpg in the 2019-2020 season?",
-    answers: [
-      { text: "Hassan Whiteside", correct: false },
-      { text: "Rudy Gobert", correct: false },
-      { text: "Andre Drummond", correct: true },
-      { text: "Giannis Antetokounmpo", correct: false },
-    ],
-  },
-  {
-    question:
-      "Who led the NBA in assists with 10.2 apg in the 2019-2020 season?",
-    answers: [
-      { text: "James Harden", correct: false },
-      { text: "Trae Young", correct: false },
-      { text: "Damian Lillard", correct: false },
-      { text: "LeBron James", correct: true },
-    ],
-  },
-  {
-    question:
-      "Who led all players in scoring with 36.3 ppg in the 2020 Postseason?",
-    answers: [
-      { text: "James Harden", correct: false },
-      { text: "Luke Doncic", correct: false },
-      { text: "Donovan Mitchell", correct: true },
-      { text: "Kawhi Leonard", correct: false },
-    ],
-  },
-];
---
+++ src/js/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question:
+      "Who led the NBA in scoring with 34.3 ppg in the 2019-2020 season?",
+    answers: [
+      { text: "James Harden", correct: true },
+      { text: "Steph Curry", correct: false },
+      { text: "Bradley Beal", correct: false },
+      { text: "Giannis Antetokounmpo", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who led the NBA in rebounding with 15.2 rpg in the 2019-2020 season?",
+    answers: [
+      { text: "Hassan Whiteside", correct: false },
+      { text: "Rudy Gobert", correct: false },
+      { text: "Andre Drummond", correct: true },
+      { text: "Giannis Antetokounmpo", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who led the NBA in assists with 10.2 apg in the 2019-2020 season?",
+    answers: [
+      { text: "James Harden", correct: false },
+      { text: "Trae Young", correct: false },
+      { text: "Damian Lillard", correct: false },
+      { text: "LeBron James", correct: true },
+    ],
+  },
+  {
+    question:
+      "Who led all players in scoring with 36.3 ppg in the 2020 Postseason?",
+    answers: [
+      { text: "James Harden", correct: false },
+      { text: "Luke Doncic", correct: false },
+      { text: "Donovan Mitchell", correct: true },
+      { text: "Kawhi Leonard", correct: false },
+    ],
+  },
+];
+
+export { questions };
