15ae2fcda6f7b94486d32a5ee87acd0dc221711f
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2021-04-12 12:09:07 -0400

    added all 10 questions

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,4 +1,5 @@
 const questions = [
+  // q1
   {
     question:
       "Who led the NBA in scoring with 34.3 ppg in the 2019-2020 season?",
@@ -9,6 +10,7 @@ const questions = [
       { text: "Giannis Antetokounmpo", correct: false },
     ],
   },
+  // q2
   {
     question:
       "Who led the NBA in rebounding with 15.2 rpg in the 2019-2020 season?",
@@ -19,6 +21,7 @@ const questions = [
       { text: "Giannis Antetokounmpo", correct: false },
     ],
   },
+  // q3
   {
     question:
       "Who led the NBA in assists with 10.2 apg in the 2019-2020 season?",
@@ -29,6 +32,7 @@ const questions = [
       { text: "LeBron James", correct: true },
     ],
   },
+  // q4
   {
     question:
       "Who led all players in scoring with 36.3 ppg in the 2020 Postseason?",
@@ -37,6 +41,70 @@ const questions = [
       { text: "Luke Doncic", correct: false },
       { text: "Donovan Mitchell", correct: true },
       { text: "Kawhi Leonard", correct: false },
+    ],
+  },
+  // q5
+  {
+    question: "What NBA player has won the most Championships in their career?",
+    answers: [
+      { text: "Bill Russell", correct: true },
+      { text: "Michael Jordan", correct: false },
+      { text: "Magic Johnson", correct: false },
+      { text: "Kareem Abdul-Jabbar", correct: false },
+    ],
+  },
+  // q6
+  {
+    question:
+      "Which former NBA player is the current all-time leader in both assists and steals?",
+    answers: [
+      { text: "Jason Kidd", correct: false },
+      { text: "LeBron James", correct: false },
+      { text: "John Stockton", correct: true },
+      { text: "Magic Johnson", correct: false },
+    ],
+  },
+  // q7
+  {
+    question: "Which NBA head coach has the most championships won as a coach?",
+    answers: [
+      { text: "Doc Rivers", correct: false },
+      { text: "Phil Jackson", correct: true },
+      { text: "Gregg Popovich", correct: false },
+      { text: "Pat Riley", correct: false },
+    ],
+  },
+  // q8
+  {
+    question:
+      "In what city were the Grizzlies located in prior to their move to Memphis?",
+    answers: [
+      { text: "Vancouver", correct: true },
+      { text: "Providence", correct: false },
+      { text: "Seattle", correct: false },
+      { text: "Staten Island", correct: false },
+    ],
+  },
+  // q9
+  {
+    question:
+      "Standing at 7 ft 7 in, who is the tallest player in NBA history?",
+    answers: [
+      { text: "Manute Bol", correct: false },
+      { text: "Shawn Bradley", correct: false },
+      { text: "Gheorghe Muresan", correct: true },
+      { text: "Yao Ming", correct: false },
+    ],
+  },
+  // q10
+  {
+    question:
+      "Follwing the US and Candada, which country represents the most current NBA players? (as of 2020)",
+    answers: [
+      { text: "Russia", correct: false },
+      { text: "Australia", correct: false },
+      { text: "France", correct: true },
+      { text: "Germany", correct: false },
     ],
   },
 ];
